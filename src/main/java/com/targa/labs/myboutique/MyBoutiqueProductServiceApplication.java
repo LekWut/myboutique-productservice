ce1e691ed308fa2eeb7c68a2dd82cf5d22deda34
Akkarapol Wuttikrangkraipol <akkarapol.wu@yipintsoi.com>
2019-03-19 11:44:59 +0700

    commit product service

--- src/main/java/com/targa/labs/myboutique/MyBoutiqueProductServiceApplication.java
+++ src/main/java/com/targa/labs/myboutique/MyBoutiqueProductServiceApplication.java
@@ -2,9 +2,11 @@ package com.targa.labs.myboutique;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableCircuitBreaker
 @EnableSwagger2
 @SpringBootApplication
 public class MyBoutiqueProductServiceApplication {
@@ -12,5 +14,5 @@ public class MyBoutiqueProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyBoutiqueProductServiceApplication.class, args);
 	}
-
+	
 }
